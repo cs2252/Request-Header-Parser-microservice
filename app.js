49d1897d38a2f1f7a7eef2197b44db36837734bc
chandan singh <chandansingh1896@gmail.com>
2018-01-19 01:12:20 +0530

    bug fixes

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
-
-var ip=require("public-ip");
 var express=require("express");
-var bodyParser =require("body-parser");
-var os=require("os");
-var networkInterface=os.networkInterfaces();
-
+var cors=require("cors");
+var bodyParser=require('body-parser');
+var useragent=require('express-useragent');
 
 var app=express();
 app.use(bodyParser.json());
-app.get("",function(req,res,next){
-	var language=req.acceptsLanguages();
-	ip.v4().then(function(args){
-			res.json({software:os.type()+"   "+os.release()+"   "+os.platform(),
-						ipaddress:args,
-						languages:language[0]
-		});
+app.use(cors());
+app.use(useragent.express());
 
-	});
+var api="/api/whoami";
+app.get(api,function(req,res,next){
+	var language=req.acceptsLanguages();
+	// var software=req.get('User-Agent');
+	// var software=req.useragent;
+	var software="OS: "+req.useragent.os+",  Browser: "+req.useragent.browser;
+	var ipaddress=req.ip;
+	res.json({language:language[0],
+				software:software,
+				ipaddress:ipaddress});
 });
+
+
+
+
+
+
+
+
+
+
 
 app.listen(3000,function(){
 	console.log("its working");
-});
+})
