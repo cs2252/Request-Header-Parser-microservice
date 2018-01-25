866a6be13ff5f7f579ed8828aacda62279144b4c
chandan singh <chandansingh1896@gmail.com>
2018-01-25 21:58:07 +0530

    recreates the projects

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-var express=require("express");
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var index = require('./routes/index');
+var users = require('./routes/users');
 var cors=require("cors");
-var bodyParser=require('body-parser');
-var useragent=require('express-useragent');
+var useragent=require("express-useragent");
 
-var app=express();
-app.use(bodyParser.json());
+
+var app = express();
 app.use(cors());
 app.use(useragent.express());
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// app.set('trust proxy',true);
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', index);
+// app.use('/users', users);
 
 app.get("*",function(req,res,next){
 	var language=req.acceptsLanguages();
@@ -19,17 +41,22 @@ app.get("*",function(req,res,next){
 				software:software,
 				ipaddress:ipaddress});
 });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-
-
-
-
-
-
-
-
-app.listen(3000,function(){
-	console.log("its working");
-})
+module.exports = app;
